cf201ddcb3358743c0d0bb58e49225ea04496eca
reymesson1 <rey_messon@hotmail.com>
2017-07-24 11:33:35 -0400

    Update registration.js

--- webPaa639/router/registration.js
+++ webPaa639/router/registration.js
@@ -47,7 +47,7 @@ module.exports = function(app,db){
             usersTable = db.getUsers();
             
 
-            res.redirect('/');
+            res.redirect('/rey');
         }
             
 
