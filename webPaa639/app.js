3420c452ccc0b82691595978ee0129e39017ace6
reymesson1 <rey_messon@hotmail.com>
2017-07-24 10:24:49 -0400

    Update app.js

--- webPaa639/app.js
+++ webPaa639/app.js
@@ -16,8 +16,8 @@ var logoutRoute = require('./router/logout.route.js');
 var esteroideHelpers = require('./lib/esteroide.js');
 var microservice = require('./lib/microservice');
 
-var db = require('./lib/db-helper.js')();
-
+var db = require('./lib/db-helper')();
+var dba = require('./lib/dba-helper');
 
 var pizza = [{"id":1,"user":"reymesson@gmail.com","items":[{ "id":"1","name":"Tradicional","price":200,"discount":"15" },{ "id":"2","name":"Mozarella","price":200,"discount":"10" },{ "id":"3","name":"BBQ","price":100,"discount":"5" },{ "id":"4","name":"Maiz","price":200,"discount":"2" }],"date":"21/07/2017 11:27:04","status":"offer","total":600}];
                 
@@ -51,7 +51,7 @@ reorderRoute(app, db);
 dealsRoute(app, db);
 neworderRoute(app, db);
 loginRoute(app, db);
-registrationRoute(app, db);
+registrationRoute(app, dba);
 checkoutRoute(app, db);
 logoutRoute(app, db);
 esteroideHelpers(app, db);
