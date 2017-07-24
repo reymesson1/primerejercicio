23b1f3e3bcbf4537777c544b5186b43809d2c334
reymesson1 <rey_messon@hotmail.com>
2017-07-24 15:29:45 -0400

    Update deals.js

--- webPaa639/router/deals.js
+++ webPaa639/router/deals.js
@@ -1,9 +1,8 @@
-
 module.exports = function(app, db){
 
 	app.get('/deals', function(req,res){
         
-        var orders = db.getOrdersFind({ "status" : "offer" });
+        var orders = db.getOrdersFind({ $and: [ { status: "active" }, { discount: { $ne: 0 } } ] });
 
 		        res.render('deals',{
 
