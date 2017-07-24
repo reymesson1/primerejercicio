59d0d80676e6015a7c9f4c880ceb42a6b6ec0939
reymesson1 <rey_messon@hotmail.com>
2017-07-24 11:29:16 -0400

    Update logout.route.js

--- webPaa639/router/logout.route.js
+++ webPaa639/router/logout.route.js
@@ -1,6 +1,6 @@
 module.exports = function(app, db){
             
-	app.get('logout', function(req,res){
+	app.get('/logout', function(req,res){
         
         db.setCookieDelete();
 		res.redirect('');
