28b74161b8e8a30371fa24f3d74512aa1eaab3fd
reymesson1 <rey_messon@hotmail.com>
2017-07-25 16:34:29 -0400

    Update deals.js

--- webPaa639/router/deals.js
+++ webPaa639/router/deals.js
@@ -1,22 +1,17 @@
 module.exports = function(app, db, dba){
 
 	app.get('/deals', function(req,res){
-        
-        //var orders = db.getOrdersFind({ $and: [ { status: "active" }, { discount: { $ne: 0 } } ] });
+                
 	var order = { $and: [ { status: "active" }, { discount: { $ne: 0 } } ] };
 		
-        dba.getOrdersFind(order, function(data){
-		console.log(data);
+		dba.getOrdersFind(order, function(data){		
+			var orders = data;
+			res.render('deals',{
+				title: 'Title',
+				name: 'Name',
+				orders: orders
+			});	
+		});
 	
-	});
-
-	/*var orders = dba.getOrdersFind({ $and: [ { status: "active" }, { discount: { $ne: 0 } } ] });
-
-		res.render('deals',{
-
-			title: 'Title',
-			name: 'Name',
-			orders: orders
-		});*/
 	});
 }
