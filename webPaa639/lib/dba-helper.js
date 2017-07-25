c215d0c93e39d80b606c66754e32eec2423cc2f7
reymesson1 <rey_messon@hotmail.com>
2017-07-25 10:06:25 -0400

    Update dba-helper.js

--- webPaa639/lib/dba-helper.js
+++ webPaa639/lib/dba-helper.js
@@ -1,10 +1,7 @@
-module.exports = function(){
-  
-    function addUser(user){
-    
+module.exports = function(){  
+    function addUser(user){    
     var MongoClient = require('mongodb').MongoClient;
     var url = "mongodb://localhost:27017/rey";
-
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       //var myobj = { email: "Company Inc", address: "Highway 37" };
@@ -13,35 +10,20 @@ module.exports = function(){
         console.log("1 record inserted");
         db.close();
       });
-    });
-  
-  }
-
-  
-  function getUsers(user,callback){
-    
+    });  
+  }  
+  function getUsers(user,callback){    
       var MongoClient = require('mongodb').MongoClient;
       var url = "mongodb://localhost:27017/rey";
-
       MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        
-          db.collection("users").find(user).toArray(function(err,result){
-            
-              callback(result);    
-          
-          })
-        
+        if (err) throw err;        
+          db.collection("users").find(user).toArray(function(err,result){            
+              callback(result);   
+          })        
         db.close();
-      });
-  
-  }  
+      });    }  
   return {
         getUsers: getUsers,
         addUser: addUser
-  }
-
-  
-  
-  
+  }  
 }
