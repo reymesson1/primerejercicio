2b45d57e2e8e22263f419c7c831853dc856936dd
reymesson1 <rey_messon@hotmail.com>
2017-07-25 15:34:50 -0400

    Update neworder.js

--- webPaa639/router/neworder.js
+++ webPaa639/router/neworder.js
@@ -136,7 +136,7 @@ module.exports = function(app, db, dba){
                 
                 }else{
                             
-                        var num = {"id":req.body.selected};
+                        var num = {"id":req.body.selected,"status":"cancelled"};
                     
                         var statusCancelled = false;
                     
