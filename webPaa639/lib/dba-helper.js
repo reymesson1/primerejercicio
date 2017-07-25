2255198ec8aa7e75a62ba9e8cd29f88c1960d17d
reymesson1 <rey_messon@hotmail.com>
2017-07-25 10:34:33 -0400

    Update dba-helper.js

--- webPaa639/lib/dba-helper.js
+++ webPaa639/lib/dba-helper.js
@@ -1,30 +1,65 @@
-module.exports = function(){  
-    function addUser(user){    
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://localhost:27017/rey";
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      //var myobj = { email: "Company Inc", address: "Highway 37" };
-      db.collection("users").insertOne(user, function(err, res) {
-        if (err) throw err;
-        console.log("1 record inserted");
-        db.close();
-      });
-    });  
-  }  
-  function getUsers(user,callback){    
+module.exports = function(){ 
+    
+    function addUser(user){  
+        
+        var MongoClient = require('mongodb').MongoClient;
+        var url = "mongodb://localhost:27017/rey";
+        MongoClient.connect(url, function(err, db) {
+          if (err) throw err;
+          //var myobj = { email: "Company Inc", address: "Highway 37" };
+          db.collection("users").insertOne(user, function(err, res) {
+            if (err) throw err;
+            console.log("1 record inserted");
+            db.close();
+          });
+        });  
+     }  
+    function getUsers(user,callback){    
+          var MongoClient = require('mongodb').MongoClient;
+          var url = "mongodb://localhost:27017/rey";
+          MongoClient.connect(url, function(err, db) {
+            if (err) throw err;        
+              db.collection("users").find(user).toArray(function(err,result){            
+                  callback(result);   
+              })        
+            db.close();
+          });    
+    }
+    
+  function addOrder(order){
+      
       var MongoClient = require('mongodb').MongoClient;
-      var url = "mongodb://localhost:27017/rey";
-      MongoClient.connect(url, function(err, db) {
-        if (err) throw err;        
-          db.collection("users").find(user).toArray(function(err,result){            
-              callback(result);   
-          })        
-        db.close();
-      });    
-  }  
+        var url = "mongodb://localhost:27017/rey";
+        MongoClient.connect(url, function(err, db) {
+          if (err) throw err;
+          //var myobj = { email: "Company Inc", address: "Highway 37" };
+          db.collection("orders").insertOne(user, function(err, res) {
+            if (err) throw err;
+            console.log("1 record inserted");
+            db.close();
+          });
+        });    
+   }
+    
+   function getOrders(){
+       
+       var MongoClient = require('mongodb').MongoClient;
+          var url = "mongodb://localhost:27017/rey";
+          MongoClient.connect(url, function(err, db) {
+            if (err) throw err;        
+              db.collection("orders").find({}).toArray(function(err,result){            
+                  callback(result);   
+              })        
+            db.close();
+          });      
+   }
+    
+    
+   
   return {
         getUsers: getUsers,
-        addUser: addUser
+        addUser: addUser,
+        getOrders: getOrders,
+        addOrder: addOrder
   }  
 }
