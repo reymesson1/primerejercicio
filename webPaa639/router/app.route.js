98c8257954c92a8271b9f43d004b2f46d4a25c91
reymesson1 <rey_messon@hotmail.com>
2017-07-25 16:40:56 -0400

    Update app.route.js

--- webPaa639/router/app.route.js
+++ webPaa639/router/app.route.js
@@ -1,18 +1,17 @@
+module.exports = function(app, db, dba){
 
-module.exports = function(app,db){
-            
-	app.get('/', function(req,res){
-        
-        var orders = db.getOrdersFind({ $and: [ { status: "active" }, { discount: { $eq: 0 } } ] });
-        
-		        res.render('index',{
-
-		                title: 'Title',
-		                name: 'Name',
-                        orders: orders,
-                        login: 'true'
-		        });
-                        
-	});                                   
-
+	app.get('/deals', function(req,res){
+                
+	var order = { $and: [ { status: "active" }, { discount: { $eq: 0 } } ] };
+		
+		dba.getOrdersFind(order, function(data){		
+			var orders = data;
+			res.render('deals',{
+				title: 'Title',
+				name: 'Name',
+				orders: orders
+			});	
+		});
+	
+	});
 }
